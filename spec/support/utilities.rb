9ccc5f3065d87cd09a067fd1dfb49523ced0e94d
Gavin White <gwhite@Gavins-MacBook-Air.local>
2014-08-24 16:51:28 -0700

    Finish exercises for chapter 5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,8 @@
+include ApplicationHelper
+
+=begin
+  # this section commented out for exercise 4 in chapter 5 section 6.
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -6,3 +11,5 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+=end
